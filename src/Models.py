ece64a67d84220b13c5c0624f5aafef9e466bfe3
THinnerichs <tilman@hinnerichs.com>
2019-09-26 11:01:27 +0200

    Merging convolutional layers

--- src/Models.py
+++ src/Models.py
@@ -317,8 +317,8 @@ class Model:
         max_pool_1_7 = layers.MaxPooling2D((2,1))(convolutional_1_7)
 
         '''
-        
-        merge_1 = layers.Concatenate(axis=1)([max_pool_1_1, max_pool_1_2, max_pool_1_3, max_pool_1_4])
+
+        merge_1 = layers.Concatenate(axis=1)([convolutional_1_1, convolutional_1_2, convolutional_1_3, convolutional_1_4])
 
         flatten = layers.Flatten()(merge_1)
         dense_1 = layers.Dense(64, activation='relu')(flatten)
