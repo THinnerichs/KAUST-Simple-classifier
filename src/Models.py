5794aad8034415f8b7f24e9a5a6e0c44e95a8dd0
THinnerichs <tilman@hinnerichs.com>
2019-10-02 09:45:09 +0200

    Improving trint model 2

--- src/Models.py
+++ src/Models.py
@@ -539,13 +539,11 @@ class Model:
         # defining model
         input_tensor = layers.Input(shape=(self.pre_length + 2 + self.post_length - 2, 64, 1))
 
-        '''
-        convolutional_1_1 = layers.Conv2D(16, kernel_size=(2, 15), activation="relu")(input_tensor)
-        max_pool_1_1 = layers.MaxPooling2D((2,1))(convolutional_1_1)
-        '''
+        convolutional_1_1 = layers.Conv2D(32, kernel_size=(2, 64), activation="relu")(input_tensor)
+        max_pool_1_1 = layers.MaxPooling2D((3,1))(convolutional_1_1)
 
         convolutional_1_2 = layers.Conv2D(32, kernel_size=(3, 64), activation='relu')(input_tensor)
-        max_pool_1_2 = layers.MaxPooling2D((2,1))(convolutional_1_2)
+        max_pool_1_2 = layers.MaxPooling2D((3,1))(convolutional_1_2)
 
         convolutional_1_3 = layers.Conv2D(32, kernel_size=(4, 64), activation='relu')(input_tensor)
         max_pool_1_3 = layers.MaxPooling2D((2, 1))(convolutional_1_3)
@@ -562,10 +560,16 @@ class Model:
         convolutional_1_7 = layers.Conv2D(32, kernel_size=(8, 64), activation='relu')(input_tensor)
         max_pool_1_7 = layers.MaxPooling2D((2,1))(convolutional_1_7)
 
-        merge_1 = layers.Concatenate(axis=1)([max_pool_1_2, max_pool_1_3, max_pool_1_4, max_pool_1_5, max_pool_1_6, max_pool_1_7])
+        merge_1 = layers.Concatenate(axis=1)([max_pool_1_1,
+                                              max_pool_1_2,
+                                              max_pool_1_3,
+                                              max_pool_1_4,
+                                              max_pool_1_5,
+                                              max_pool_1_6,
+                                              max_pool_1_7])
 
         flatten = layers.Flatten()(merge_1)
-        dense_1 = layers.Dense(512, activation='relu')(flatten)
+        dense_1 = layers.Dense(128, activation='relu')(flatten)
         output_tensor = layers.Dense(1, activation='sigmoid')(dense_1)
 
         model = models.Model(input_tensor, output_tensor)
