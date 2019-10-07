2edb0ab41c4bff04cf3f00f88bafe44e552981e9
THinnerichs <tilman@hinnerichs.com>
2019-10-07 09:04:38 +0200

    Testing voting classifier 10

--- src/Voting_classifier.py
+++ src/Voting_classifier.py
@@ -55,11 +55,11 @@ class Voting_classifer:
         for round in range(1,11):
             weights = np.array([1,1,1,1,1,1,1,1,1,1])
 
-            matrix = np.array([]).reshape((20000,))
+            matrix = np.array([])
             for i in range(len(self.datasets)):
                 array = self.data_dict[round][self.datasets[i]]
                 array = array.reshape((array.shape[0],))
-                matrix = np.vstack((matrix, array))
+                matrix = np.vstack((matrix, array)) if matrix.size else array
 
             matrix = (np.transpose(matrix) > 0.5).astype(int)
             y_pred = matrix.dot(weights)
