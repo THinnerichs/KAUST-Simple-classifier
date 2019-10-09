cae4655d568809d1c21ab20cd940ce308b29bb9b
THinnerichs <tilman@hinnerichs.com>
2019-10-09 13:05:45 +0200

    Running Minimize weight loss 24

--- src/Voting_classifier.py
+++ src/Voting_classifier.py
@@ -161,10 +161,11 @@ class Voting_classifer:
         if hard:
             matrix = (matrix > 0.5).astype(int)
 
+        print("MATRIX:", matrix)
+
         y_pred = matrix.dot(weights)
 
-        # y_pred = (np.divide(y_pred, weights.sum()) > 0.5).astype(int)
-        y_pred = (y_pred > 0.5).astype(int)
+        y_pred = (np.divide(y_pred, weights.sum()) > 0.5).astype(int)
 
         y_true = self.data_dict["y_data"][self.test_indizes[round]]
 
@@ -183,14 +184,16 @@ class Voting_classifer:
             print("Round", round)
 
             x0 = np.array([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
-            objective_fct = lambda array: self.objective_fct_vote(array, round=round, hard=False)
-            res = minimize(objective_fct, x0=x0, method='Nelder-Mead')
-            weights = res.x
+            objective_fct = lambda array: self.objective_fct_vote(array, round=round, hard=True)
+            # res = minimize(objective_fct, x0=x0, method='Nelder-Mead')
+            # weights = res.x
 
             print("Test objective function")
-            print(weights)
-            print("minimized weights", objective_fct(weights))
+            # print(weights)
+            # print("minimized weights", objective_fct(weights))
             print("My weights", objective_fct(np.array([5,5,5,4,4,3,1,1,1,1,1,1,1,1,1])))
+
+            raise Exception
 
             print("TRAINING PERFORMANCE:")
             train_matrix = np.array([])
